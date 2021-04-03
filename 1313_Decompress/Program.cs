a166c797a0112b8a0805c4692a9c4bf3653fdc46
David Chui <david.gchui@gmail.com>
2021-04-03 10:40:18 -0600

    Add files via upload
    Decompresses an input array of [freq, val] = [nums[2*i], nums[2*i+1]]

---
+++ 1313_Decompress/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+//another challenge: how to tell if an integer is a pallindrome
+
+namespace _1313_Decompress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = { 1, 1, 2, 3, 4, 2};
+            int[] resultArray = Decompress(testArray);
+            foreach (int x in resultArray)
+            {
+                Console.Write(x + " ");
+            }
+        }
+        public static int[] Decompress(int[] nums)
+        {
+            //initializing result array to the correct length
+            int l = 0;
+            for (int i = 0; i < nums.Length; i+= 2)
+            {
+                l += nums[i];
+            }
+            int[] result = new int[l];
+
+            //
+            int temp = 0;
+            for (int i = 0; i < nums.Length; i+= 2)
+            {
+                int j = i + 1;
+                int freq = nums[i];
+                int value = nums[j];
+                for (int k = temp; k < temp + freq; k++)
+                {
+                    result[k] = value; 
+                }
+                temp += freq;
+            }
+
+            return result;
+        }
+    }
+}
