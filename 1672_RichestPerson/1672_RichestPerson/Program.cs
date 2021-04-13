554e62ccbecf9e621c485f114a2a6ac05a01ec96
David Chui <david.gchui@gmail.com>
2021-04-13 09:27:29 -0600

    Add files via upload

---
+++ 1672_RichestPerson/1672_RichestPerson/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1672_RichestPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int MaxWealth(int[][] accounts)
+        {
+            int max = 0;
+            for (int j = 0; j < accounts.Length; j++)
+            {
+                int total = 0;
+                for (int k = 0; k < accounts[j].Length; k++)
+                {
+                    total = total + accounts[j][k];
+                    if (max < total)
+                    {
+                        max = total;
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
